30d3202a39767aa546edbbc84ad8d5ff8edd47e9
Robert Pallas <robert@devtailor.com>
2018-04-18 00:16:44 +0300

    major: Hapi17
    - make plugin compatible with breaking Hapi changes
    - update pg-promise to latest
    - clean tests
    - reflect change in README

--- test/index.js
+++ test/index.js
@@ -3,8 +3,6 @@
 const Lab = require('lab');
 const Code = require('code');
 const Hapi = require('hapi');
-const Proxyquire = require('proxyquire');
-
 
 let connect = function () {
 
@@ -14,89 +12,56 @@ let connect = function () {
     });
 };
 
-const stub = {
-    'pg-promise': function () {
+const Plugin = {
+    name: 'pg-promise',
+    register(server, options) {
 
         return function () {
 
             return {
-                connect: connect
+                connect
             };
         };
     }
 };
-const Plugin = Proxyquire('../', stub);
 const lab = exports.lab = Lab.script();
-let request;
+const request = {
+    method: 'GET',
+    url: '/'
+};
 let server;
 
+lab.beforeEach(() => {
 
-lab.beforeEach((done) => {
-
-    server = new Hapi.Server();
-    server.connection({ port: 0 });
+    server = Hapi.Server({ port: 0 });
     server.route({
         method: 'GET',
         path: '/',
-        handler: function (req, reply) {
+        handler: function (req) {
 
-            /*if (req.query.kill) {
-                req.pg.kill = true;
-            }*/
-
-            reply('hapi-pg-promise, at your service');
+            return 'hapi-pg-promise, at your service';
         }
     });
-
-    request = {
-        method: 'GET',
-        url: '/'
-    };
-
-    done();
 });
 
 
 lab.experiment('Postgres Plugin', () => {
 
-    lab.test('it registers the plugin', (done) => {
+    lab.test('it registers the plugin', async () => {
 
-        server.register(Plugin, (err) => {
+        try {
 
-            server.start(() => {
+            await server.register(Plugin);
+            await server.start();
+        }
+        catch (err) {
 
-                Code.expect(err).to.not.exist();
-                done();
-            });
-        });
+            Code.fail(err);
+        }
     });
 
 
-    /*lab.test('it returns an error when the connection fails in the extension point', (done) => {
-
-        const realConnect = connect;
-        connect = function(resolve, reject) {
-            return new Promise(function(resolve, reject) {
-                reject(Error('connect failed'));
-            });
-        };
-
-        server.register(Plugin, (err) => {
-
-            server.start(() => {});
-            Code.expect(err).to.not.exist();
-
-            server.inject(request, (response) => {
-
-                Code.expect(response.statusCode).to.equal(500);
-                connect = realConnect;
-
-                done();
-            });
-        });
-    });*/
-
-    lab.test('it successfully returns when the connection succeeds in extension point', (done) => {
+    lab.test('it successfully returns when the connection succeeds in extension point', async () => {
 
         const realConnect = connect;
         connect = function () {
@@ -107,79 +72,38 @@ lab.experiment('Postgres Plugin', () => {
             });
         };
 
-        // const realConnect = stub.pg.connect;
-        // stub.pg.connect = function (connection, callback) {
-        //
-        //     const returnClient = () => {};
-        //
-        //     callback(null, {}, returnClient);
-        // };
+        try {
 
-        server.register(Plugin, (err) => {
+            await server.register(Plugin);
+            const response = await server.inject(request);
+            Code.expect(response.statusCode).to.equal(200);
+            connect = realConnect;
+        }
+        catch (err) {
 
-            Code.expect(err).to.not.exist();
-
-            server.inject(request, (response) => {
-
-                Code.expect(response.statusCode).to.equal(200);
-                connect = realConnect;
-
-                done();
-            });
-        });
+            Code.fail(err);
+        }
     });
 
-
-    /*lab.test('it successfully cleans up during the server tail event', (done) => {
-
-        const realConnect = stub.pg.connect;
-        stub.pg.connect = function (connection, callback) {
-
-            const returnClient = function (killSwitch) {
-
-                Code.expect(killSwitch).to.equal(true);
-                stub.pg.connect = realConnect;
-
-                done();
-            };
-
-            callback(null, {}, returnClient);
-        };
-
-        server.register(Plugin, (err) => {
-
-            Code.expect(err).to.not.exist();
-
-            request.url = '/?kill=true';
-
-            server.inject(request, (response) => {
-
-                Code.expect(response.statusCode).to.equal(200);
-                stub.pg.connect = realConnect;
-            });
-        });
-    });*/
-
-
-    lab.test('it successfully uses native bindings without error', (done) => {
+    lab.test('it successfully uses native bindings without error', async () => {
 
         const pluginWithConfig = {
-            register: Plugin,
             options: {
                 connectionString: 'postgres://postgres:mysecretpassword@localhost/hapi_node_postgres',
                 native: true
-            }
+            },
+            ...Plugin
         };
+        
+        try {
 
-        server.register(pluginWithConfig, (err) => {
+            await server.register(pluginWithConfig);
+            const response = await server.inject(request);
+            Code.expect(response.statusCode).to.equal(200);
+        }
+        catch (err) {
 
-            Code.expect(err).to.not.exist();
-
-            server.inject(request, (response) => {
-
-                Code.expect(response.statusCode).to.equal(200);
-                done();
-            });
-        });
+            Code.fail(err);
+        }
     });
 });
